36861445e249f783ffb57ec8164b57ace7da4c11
Samy Barakat <sbarakat2@mail.sfsu.edu>
2021-05-11 16:42:24 -0700

    experimenting with basics of pointers

---
+++ pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+int a = 1025;
+int *p;
+p = &a;
+printf("size of integer is %d\n", sizeof(int));
+printf("Address = %d, value = %d\n", p, *p);
+printf("Address = %d, value = %d\n", p+1, *(p+1));
+char *p0;
+p0 = (char*)p;
+printf("Address = %d, value = %d\n", p0, *p0);
+printf("Address = %d, value = %d\n", p0+1, *(p0+1));
+
+}
